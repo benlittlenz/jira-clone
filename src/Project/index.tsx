8d44be6d526aa51a64ba003a3d10caf7c74ca639
Ben Little <ben.little@archipro.co.nz>
2022-01-31 17:57:53 +1300

    Feat: Added form component

--- src/Project/index.tsx
+++ src/Project/index.tsx
@@ -1,12 +1,25 @@
-import { ProjectLeftNav } from '../components/LeftNav';
-import { ProjectSidebar } from '../components/Sidebar/Sidebar';
-import { ProjectPage } from './Project.styled';
+import React from "react";
+import { Route, Redirect, useRouteMatch, useHistory } from "react-router-dom";
+
+import { ProjectLeftNav } from "../components/LeftNav";
+import { ProjectSidebar } from "../components/Sidebar/Sidebar";
+import { ProjectPage } from "./Project.styled";
+import { Settings } from "./Settings";
 
 export const Project = () => {
+  const match = useRouteMatch();
+  const history = useHistory();
   return (
     <ProjectPage>
       <ProjectLeftNav />
       <ProjectSidebar />
+
+      <Route
+        path={`${match.path}/settings`}
+        render={() => (
+          <Settings />
+        )}
+      />
     </ProjectPage>
   );
-}
+};
