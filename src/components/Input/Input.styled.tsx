56cea20db34da6b79276438557a91ff14c8b6350
Ben Little <ben.little@archipro.co.nz>
2022-01-31 16:28:54 +1300

    Feat: Added input component

---
+++ src/components/Input/Input.styled.tsx
@@ -0,0 +1,30 @@
+import styled, { css } from "styled-components";
+import { font, color } from "../../shared/styles";
+
+export const StyledInput = styled.div`
+  position: relative;
+  display: inline-block;
+  height: 32px;
+  width: 100%;
+`;
+
+export const InputElement = styled.input`
+  height: 100%;
+  width: 100%;
+  padding: 0 7px;
+  border-radius: 3px;
+  border: 1px solid ${color.borderLightest}
+  color: ${color.textDarkest};
+  background: ${color.backgroundLightest};
+  transition: background 0.1s;
+  ${font.regular};
+  ${font.size(15)};
+  &:hover {
+    background: ${color.backgroundLight};
+  }
+  &:focus {
+    background: #fff;
+    border: 1px solid ${color.borderInputFocus};
+    box-shadow: 0 0 0 1px ${color.borderInputFocus};
+  }
+`;
