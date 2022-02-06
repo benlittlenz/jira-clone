053d2a6fe18df6d8c92d313217b295fd20041220
Ben Little <ben.little@archipro.co.nz>
2022-02-06 20:15:48 +1300

    Added field wrapper component

---
+++ src/components/Form/Fields/Wrapper.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { FieldError } from "react-hook-form"
+import styled from "styled-components"
+import { font } from "../../../shared/styles";
+
+interface FieldWrapperProps {
+  children: React.ReactNode;
+  error?: FieldError | undefined;
+}
+
+const StyledWrapper = styled.div`
+  margin: 6px 0 8px 0;
+`;
+
+const ErrorMessage = styled.p`
+  ${font.size(11)};
+  font-weight: 500;
+  color: red;
+`
+
+export const Wrapper = ({children, error}: FieldWrapperProps) => {
+  return (
+    <StyledWrapper>
+      {children}
+      {error?.message && <ErrorMessage role="alert" aria-label="error-message">{error.message}</ErrorMessage>}
+    </StyledWrapper>
+  );
+}
