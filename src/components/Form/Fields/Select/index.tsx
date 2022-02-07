de2b2a64f33c50050f3579aafa5b1d8153ba1430
Ben Little <ben.little@archipro.co.nz>
2022-02-07 16:54:41 +1300

    Added useonclickoutside hook to close dropdown

--- src/components/Form/Fields/Select/index.tsx
+++ src/components/Form/Fields/Select/index.tsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useRef, useState} from "react";
+import { useOnClickOutside } from "../../../../hooks/useOnClickOutside";
 import { FieldWrapper } from "../Wrapper";
 import { SelectDropdown } from "./Dropdown";
 import { StyledSelect, ValueContainer } from "./Dropdown.styled";
 import { SelectProps } from "./types";
 
 export const Select: React.FC<SelectProps> = ({ options }) => {
-  const [isDropdownOpen, setIsDropdownOpen] = React.useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const selectRef = useRef<HTMLDivElement>(null);
+
+  const deactiveDropdown = () => {
+    setIsDropdownOpen(false);
+    if(selectRef && selectRef.current) {
+      selectRef?.current?.focus();
+   }
+  };
 
   const activateDropdown = () => {
     setIsDropdownOpen(true);
   };
+
+  useOnClickOutside(selectRef, deactiveDropdown);
   return (
     <FieldWrapper>
-      <StyledSelect>
+      <StyledSelect ref={selectRef}>
         <ValueContainer data-testid='select' onClick={activateDropdown}>{isDropdownOpen && <SelectDropdown options={options}/>}</ValueContainer>
       </StyledSelect>
     </FieldWrapper>
